d86616660b25ef141bf1a14b5633af5b7a5869d5
unknown <yamen.github1@gmail.com>
2022-09-13 03:10:46 -0700

    n

--- lib/PresentationLayer/Widgets/otp_widgets.dart
+++ lib/PresentationLayer/Widgets/otp_widgets.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:noble/Constants/font_styles.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
+
+import '../../Constants/colors.dart';
 
 /* import 'package:noble/Constants/colors.dart';
 
@@ -14,8 +17,26 @@ class Verify extends StatelessWidget {
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.ltr,
-      child:
-          PinCodeTextField(appContext: context, length: 6, onChanged: (_) {}),
+      child: PinCodeTextField(
+        pinTheme: PinTheme(
+            shape: PinCodeFieldShape.box,
+            borderRadius: BorderRadius.circular(10),
+            fieldHeight: 50,
+            fieldWidth: 50,
+            selectedColor: AppColors.orange,
+            activeFillColor: Colors.white,
+            inactiveFillColor: AppColors.lightwhite,
+            inactiveColor: AppColors.lightgrey,
+            selectedFillColor: AppColors.lightwhite,
+            activeColor: AppColors.lightgrey),
+        appContext: context,
+        length: 6,
+        onChanged: (_) {},
+        autoFocus: true,
+        cursorColor: AppColors.blue,
+        textStyle: blue18NoBold,
+        keyboardType: TextInputType.number,
+      ),
     );
   }
 }
